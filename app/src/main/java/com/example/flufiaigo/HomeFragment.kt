d8b433ec81a1c0a653edbc1311dbd674679b0be8
Daniel López del Burgo <130463781+Htaga@users.noreply.github.com>
2026-03-25 02:02:51 +0100

    feat: añadir botones y navegacion en pantalla principal

--- app/src/main/java/com/example/flufiaigo/HomeFragment.kt
+++ app/src/main/java/com/example/flufiaigo/HomeFragment.kt
@@ -1,16 +1,34 @@
 package com.example.flufiaigo
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
 
 class HomeFragment : Fragment() {
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         return inflater.inflate(R.layout.fragment_home, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val btnMovimiento = view.findViewById<Button>(R.id.btnNuevoMovimiento)
+        val btnNomina = view.findViewById<Button>(R.id.btnNuevaNomina)
+
+        btnMovimiento.setOnClickListener {
+            findNavController().navigate(R.id.action_homeFragment_to_movimientoFragment)
+        }
+
+        btnNomina.setOnClickListener {
+            findNavController().navigate(R.id.action_homeFragment_to_nominaFragment)
+        }
     }
 }
